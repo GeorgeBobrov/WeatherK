59b07f50c310ff847f0f2968dc41e340576f30ea
George Bobrov <skivvvvy@gmail.com>
2021-12-08 21:59:18 +0200

    Delete OkHttp and Gson

--- app/src/main/java/com/example/WeatherK/MainActivity.kt
+++ app/src/main/java/com/example/WeatherK/MainActivity.kt
@@ -6,24 +6,18 @@ import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
-import com.google.gson.Gson
-import com.google.gson.JsonSyntaxException
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
 import java.util.*
-
-import okhttp3.*
-import okhttp3.HttpUrl.Companion.toHttpUrl
-import java.io.IOException
 
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.features.*
+import io.ktor.client.features.json.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.client.statement.HttpResponse
-import io.ktor.utils.io.charsets.*
 
 import kotlinx.coroutines.*
 
@@ -43,7 +37,9 @@ class MainActivity : AppCompatActivity() {
 		textResponse.setMovementMethod(ScrollingMovementMethod())
 	}
 
-	private val okHttpClient = OkHttpClient()
+	val KtorClient = HttpClient(CIO){
+		install(JsonFeature)
+	}
 
 	fun buttonQueryClick(sender: View?) {
 		editCity.clearFocus()
@@ -51,19 +47,15 @@ class MainActivity : AppCompatActivity() {
 		val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
 		imm.hideSoftInputFromWindow(linearLayoutH.windowToken, 0)
 
-		val urlBuilder = (baseURLRemote + "weather").toHttpUrl().newBuilder()
-		urlBuilder.addQueryParameter("q", editCity.text.toString())
-		urlBuilder.addQueryParameter("appid", APIkey)
-		val url = urlBuilder.build().toString()
-
-
 		GlobalScope.async() {
-			val KtorClient = HttpClient(CIO)
-			val statement: HttpStatement = KtorClient.get(url)
+			val statement: HttpStatement = KtorClient.get(baseURLRemote + "weather") {
+				parameter("q", editCity.text.toString())
+				parameter("appid", APIkey)
+			}
 			statement.execute { response: HttpResponse ->
 				try {
-					val stringBody: String = response.receive()
-					runOnUiThread { processWeatherCity(stringBody) }
+					val weatherCity: ResponseWeatherCity = response.receive()
+					runOnUiThread { processWeatherCity(weatherCity) }
 				} catch (cre: ClientRequestException) {
 					val stringBody: String = cre.response.receive()
 					runOnUiThread { textResponse.text = stringBody }
@@ -71,84 +63,49 @@ class MainActivity : AppCompatActivity() {
 			}
 		}
 
-//		val request = Request.Builder()
-//			.url(url)
-//			.build()
-
-//		okHttpClient.newCall(request).enqueue(object : Callback {
-//			override fun onFailure(call: Call, e: IOException) {
-//				call.cancel()
-//			}
-//
-//			override fun onResponse(call: Call, response: Response) {
-//				val stringBody = response.body!!.string()
-//				if (response.code == 200)
-//					runOnUiThread { processWeatherCity(stringBody) }
-//				else
-//					runOnUiThread { textResponse.text = stringBody }
-//			}
-//		})
 	}
 
-	fun processWeatherCity(weather: String?) {
-		val gson = Gson()
-		try {
-			val weatherCity = gson.fromJson(weather, ResponseWeatherCity::class.java)
-			val weatherDescription = weatherCity.weather[0].main
-			val time = Date(weatherCity.dt * 1000L)
-			val innerText = "Now, ${time.toLocaleString()} in ${weatherCity.name} " +
+	fun processWeatherCity(weatherCity: ResponseWeatherCity) {
+		val weatherDescription = weatherCity.weather[0].main
+		val time = Date(weatherCity.dt * 1000L)
+		val innerText = "Now, ${time.toLocaleString()} in ${weatherCity.name} " +
 				"is ${normalizeTemp(weatherCity.main.temp)} °C, $weatherDescription"
 
-			textResponse.text = innerText
-			//            textResponse.append("\n Humidity " + weatherCity.main.humidity);
-			queryWeatherForecast(weatherCity)
-		} catch (e: JsonSyntaxException) {
-			Log.d("JsonSyntaxException", e.localizedMessage)
-		}
+		textResponse.text = innerText
+		//            textResponse.append("\n Humidity " + weatherCity.main.humidity);
+		queryWeatherForecast(weatherCity)
 	}
 
 	fun queryWeatherForecast(a_weatherCity: ResponseWeatherCity) {
-		val urlBuilder = (baseURLRemote + "onecall").toHttpUrl().newBuilder()
-		urlBuilder.addQueryParameter("lat", a_weatherCity.coord.lat.toString())
-		urlBuilder.addQueryParameter("lon", a_weatherCity.coord.lon.toString())
-		urlBuilder.addQueryParameter("appid", APIkey)
-		val url = urlBuilder.build().toString()
 
-		val request = Request.Builder()
-			.url(url)
-			.build()
-
-		okHttpClient.newCall(request).enqueue(object : Callback {
-			override fun onFailure(call: Call, e: IOException) {
-				call.cancel()
+		GlobalScope.async() {
+			val statement: HttpStatement = KtorClient.get(baseURLRemote + "onecall") {
+				parameter("lat", a_weatherCity.coord.lat.toString())
+				parameter("lon", a_weatherCity.coord.lon.toString())
+				parameter("appid", APIkey)
 			}
 
-			@Throws(IOException::class)
-			override fun onResponse(call: Call, response: Response) {
-				val stringBody = response.body!!.string()
-				if (response.code == 200)
-					runOnUiThread { processWeatherForecast(stringBody) }
-				else
+			statement.execute { response: HttpResponse ->
+				try {
+					val weatherForecast: ResponseWeatherForecast = response.receive()
+					runOnUiThread { processWeatherForecast(weatherForecast) }
+				} catch (cre: ClientRequestException) {
+					val stringBody: String = cre.response.receive()
 					runOnUiThread { textResponse.text = stringBody }
+				}
 			}
-		})
+		}
+
 	}
 
-	fun processWeatherForecast(weather: String?) {
-		val gson = Gson()
-		try {
-			val weatherForecast = gson.fromJson(weather, ResponseWeatherForecast::class.java)
-
+	fun processWeatherForecast(weatherForecast: ResponseWeatherForecast) {
 //            Date time = new Date(weatherCity.dt * 1000l);
 
-			textResponse.append("\nHourly:")
+		textResponse.append("\nHourly:")
 
-			for (hourForecast in weatherForecast.hourly) {
-				val innerText = addHourForecast(hourForecast)
-				textResponse.append("\n $innerText")
-			}
-		} catch (e: JsonSyntaxException) {
-			Log.d("JsonSyntaxException", e.localizedMessage)
+		for (hourForecast in weatherForecast.hourly) {
+			val innerText = addHourForecast(hourForecast)
+			textResponse.append("\n $innerText")
 		}
 	}
 
